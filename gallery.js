e46683f65633feb48c1e9d411ffd89bc3649d727
CongHuy-hash <huydinh2512007@gmail.com>
2026-07-29 17:25:28 +0700

    Create gallery.js

---
+++ gallery.js
@@ -0,0 +1,17 @@
+function upDate(previewPic) {
+  console.log("Event triggered");
+  console.log("Alt text: " + previewPic.alt);
+  console.log("Image source: " + previewPic.src);
+
+  let imageDiv = document.getElementById("image");
+
+  imageDiv.innerHTML = previewPic.alt;
+  imageDiv.style.backgroundImage = "url('" + previewPic.src + "')";
+}
+
+function undo() {
+  let imageDiv = document.getElementById("image");
+
+  imageDiv.style.backgroundImage = "url('')";
+  imageDiv.innerHTML = "Hover over an image below to display here.";
+}
